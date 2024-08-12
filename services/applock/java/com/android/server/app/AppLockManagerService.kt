56438fc519e2fddab1d32d0dfdd701b81cc9eac6
imranpopz <imranpopz32@gmail.com>
2024-08-12 04:44:57 +0000

    Revert "AppLock: Allow locking all apps visible in launcher"
    This reverts commit f76eb6086b567dbc3b397c0de0c6c457dbbc6c2c.

--- services/applock/java/com/android/server/app/AppLockManagerService.kt
+++ services/applock/java/com/android/server/app/AppLockManagerService.kt
@@ -50,7 +50,6 @@ import android.util.Slog
 
 import com.android.internal.R
 import com.android.internal.annotations.GuardedBy
-import com.android.internal.util.aospaplus.Utils
 import com.android.server.LocalServices
 import com.android.server.SystemService
 import com.android.server.app.AppLockManagerServiceInternal
@@ -480,7 +479,6 @@ class AppLockManagerService(
                 userId
             )
             if (!aInfo.isSystemApp()) return
-            if (Utils.launchablePackages(context).contains(pkg)) return
             if (!whiteListedSystemApps.contains(pkg))
                 throw IllegalArgumentException("System package $pkg is not whitelisted")
         } catch(e: PackageManager.NameNotFoundException) {
